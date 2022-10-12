543a9eda6ea8c13e4835409a31d402df2b9a20e1
Juwan <jhollingsworth5@student.clayton.edu>
2022-10-12 01:34:06 -0400

    Add grid cards, styling, & placeholder imgs

--- src/containers/grid/Grid.jsx
+++ src/containers/grid/Grid.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./grid.css";
 import Image1 from "../../assets/content_img1.jpg";
-
+import Image2 from "../../assets/content_img2.jpg";
+import Image3 from "../../assets/content_img3.png";
 function Grid() {
   // add card div
   // add title
@@ -9,13 +10,34 @@ function Grid() {
   // add Text
   // add button
   return (
-    <div className="grid__list">
+    <div className="grid__container">
       <div className="card">
         <div className="title">Project 1</div>
         <div className="image">
           <img src={Image1} />
         </div>
         <div className="text">Project description</div>
+        <button className="view__btn">View Project</button>
+      </div>
+
+      {/* second card */}
+      <div className="card">
+        <div className="title">Project 2</div>
+        <div className="image">
+          <img src={Image2} />
+        </div>
+        <div className="text">Project description</div>
+        <button className="view__btn">View Project</button>
+      </div>
+
+      {/* third card */}
+      <div className="card">
+        <div className="title">Project 3</div>
+        <div className="image">
+          <img src={Image3} />
+        </div>
+        <div className="text">Project description</div>
+        <button className="view__btn">View Project</button>
       </div>
     </div>
   );
