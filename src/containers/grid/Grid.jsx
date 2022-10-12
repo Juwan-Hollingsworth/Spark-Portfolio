9c3eb822e2dfd7bb32f2079edf846bf9106077b7
Juwan <jhollingsworth5@student.clayton.edu>
2022-10-12 00:04:01 -0400

    implement grid

--- src/containers/grid/Grid.jsx
+++ src/containers/grid/Grid.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "./grid.css";
+import Image1 from "../../assets/content_img1.jpg";
 
 function Grid() {
-  return <div>Grid</div>;
+  // add card div
+  // add title
+  // add image
+  // add Text
+  // add button
+  return (
+    <div className="grid__list">
+      <div className="card">
+        <div className="title">Project 1</div>
+        <div className="image">
+          <img src={Image1} />
+        </div>
+        <div className="text">Project description</div>
+      </div>
+    </div>
+  );
 }
 
 export default Grid;
