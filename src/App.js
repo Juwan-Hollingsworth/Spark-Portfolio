425062850c168b690ae1ba9ebb32c6c70fddd34f
Juwan <jhollingsworth5@student.clayton.edu>
2022-11-25 23:57:20 -0500

    Add footer section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-import { Grid, Header } from "./containers";
+import { Grid, Header, Footer } from "./containers";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Grid />
+      <Footer />
     </div>
   );
 }
