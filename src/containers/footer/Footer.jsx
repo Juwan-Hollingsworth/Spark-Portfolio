04ed7438efc2f76b4e8c4c4c614253d259bfe1e9
Juwan <jhollingsworth5@student.clayton.edu>
2022-11-25 23:57:45 -0500

    footer section WIP

---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div>
+      <div>
+        <hr />
+        <div className="row">
+          <div className="column">
+            <div>A</div>
+          </div>
+          <div className="column">B</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,3 +1,5 @@
 export { default as Header } from "./header/Header";
 
 export { default as Grid } from "./grid/Grid";
+
+export { default as Footer } from "./footer/Footer";
