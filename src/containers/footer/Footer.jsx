c1cd6b982f1d9b02c04bb638fda8dd0a8411daa3
Juwan <jhollingsworth5@student.clayton.edu>
2023-02-01 17:59:21 -0500

    rename + style classes WIP

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -4,31 +4,30 @@ import Certs from "../../assets/cert_badges.png";
 
 function Footer() {
   return (
-    <div className="footer__background">
+    <div className="footer__main">
       <div>
         <hr />
-        <div className="row">
-          <div className="column">
-            <h3>Reviews</h3>
-          </div>
-          <div className="column">
-            <div>
-              <h3>Industry Specialist</h3>
-              <p>We are DBE, MBE, SBE, AABE, and FBE certified.</p>
+        <div className="row__main">
+          <div className="col__main">
+            <div className="col1">
+              <h3>Reviews</h3>
             </div>
-            <img src={Certs} />
-          </div>
-          <div className="column">
-            <h3>Get In Touch</h3>
-            <p>
-              384 Northyards Blvd. NW Suite 190 Atlanta, GA 30313 <br /> 1720 I
-              St NW 4th Floor Washington DC 20006 <br />
-              Monday – Friday 9am – 6pm
-              <br />
-              (404) 850-4102 (Spark)
-              <br />
-              hello@sparksocial.agency
-            </p>
+            <div className="col2">
+              <div>
+                <h3>Industry Specialist</h3>
+                <h4>We are DBE, MBE, SBE, AABE, and FBE certified.</h4>
+              </div>
+              <img src={Certs} />
+            </div>
+            <div className="col3">
+              <h3>Get In Touch</h3>
+              <h4>384 Northyards Blvd. NW Suite 190 Atlanta, GA 30313 </h4>
+              <h4>1720 I St NW 4th Floor Washington DC 20006 </h4>
+              <h4>Monday – Friday 9am – 6pm</h4>
+              <h4>(404) 850-4102 (Spark)</h4>
+              <h4>hello@sparksocial.agency</h4>
+              <div>social media buttons</div>
+            </div>
           </div>
         </div>
       </div>
